08c2211764acc77362070f55105f128034614123
Dan Paulat <dpaulat@gmail.com>
2017-12-17 16:20:25 -0700

    XMP Field Configuration

--- config.php
+++ config.php
@@ -73,6 +73,61 @@ EOT;
 EOT;
 
     endtable();
+
+    echo '<br>' . $LINEBREAK;
+
+    $plugin_help = $extensible_metadata->help_button('xmp_fields');
+    starttable('100%', $lang_plugin_extensible_metadata['config_name'] . " - " . $lang_plugin_extensible_metadata['config_xmp_fields'] . $plugin_help, 4);
+
+    echo <<<EOT
+	<tr>
+		<th class="tableh2">{$lang_plugin_extensible_metadata['config_field_name']}</th>
+		<th class="tableh2">{$lang_plugin_extensible_metadata['config_display_name']}</th>
+		<th class="tableh2">{$lang_plugin_extensible_metadata['config_displayed']}</th>
+		<th class="tableh2">{$lang_plugin_extensible_metadata['config_indexed']}</th>
+	</tr>
+EOT;
+
+    $xmp_fields = $extensible_metadata->xmp_fields();
+    $alternate = false;
+    foreach ($xmp_fields as $xmp_field) {
+        $class = $alternate ? "tableb tableb_alternate" : "tableb";
+        $alternate = !$alternate;
+        $display_name = htmlspecialchars($xmp_field['display_name']);
+        $displayed = ($xmp_field['displayed'] == 1) ? 'checked="checked"' : '';
+        $indexed = ($xmp_field['indexed'] == 1) ? 'checked="checked"' : '';
+        echo <<<EOT
+		<tr>
+			<td class="{$class}">
+				{$xmp_field['name']}
+			</td>
+			<td class="{$class}">
+				<div style="width: 100%; display: table;">
+					<input type="text" name="" id="" value="{$display_name}" style="display: table-cell; width: 100%;" />
+				</div>
+			</td>
+			<td class="{$class}" style="text-align: center;">
+				<input type="checkbox" name="displayed" id="displayed" value="{$xmp_field['id']}" {$displayed} />
+			</td>
+			<td class="{$class}" style="text-align: center;">
+				<input type="checkbox" name="indexed" id="indexed" value="{$xmp_field['id']}" {$indexed} />
+			</td>
+		</tr>
+EOT;
+    }
+    $ok_icon = cpg_fetch_icon('ok');
+    $class = $alternate ? "tableb tableb_alternate" : "tableb";
+    echo <<<EOT
+	<tr>
+		<td class = "{$class}" colspan="4">
+			<button type="button" id="xmp-save-fields">{$ok_icon}{$lang_plugin_extensible_metadata['config_apply']}</button>
+		</td>
+</td>
+	</tr>
+EOT;
+
+
+    endtable();
 }
 
 xmp_config_form();
