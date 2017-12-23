e14eb5cf51fd4d9bec2a1cd8abba29e39c3fefc1
Dan Paulat <dpaulat@gmail.com>
2017-12-23 11:52:24 -0700

    Commenting refresh operation

--- refresh.php
+++ refresh.php
@@ -49,7 +49,7 @@ function xmp_refresh_process()
     if ($page === false) {
         $page = 0;
     }
-    $limit = 100;
+    $limit = 10;
     $start = $page * $limit;
     $end = $start + $limit;
     $total_images = $extensible_metadata->total_images();
@@ -57,6 +57,7 @@ function xmp_refresh_process()
     $xmp_files_created = 0;
     $xmp_files_skipped = 0;
 
+    // If the requested start image is beyond the number of total images, the process is complete
     if ($start >= $total_images) {
         $data = array(
             'status'      => 'success',
@@ -66,13 +67,14 @@ function xmp_refresh_process()
     }
 
     // Get pictures from database
-    $result = cpg_db_query(
+    $pictures = cpg_db_query(
         "SELECT `pid`, `filepath`, `filename`
          FROM {$CONFIG['TABLE_PICTURES']}
          ORDER BY `pid` ASC
          LIMIT {$start}, {$limit}");
 
-    if (!$result) {
+    // If there are no pictures returned, there is nothing to process
+    if (!$pictures) {
         $data = array(
             'status'       => 'error',
             'error_reason' => 'Database error');
@@ -80,35 +82,54 @@ function xmp_refresh_process()
         return;
     }
 
-    // Find new fields in XMP metadata
-    while (($row = $result->fetchAssoc()) !== NULL) {
-        $xmp_elements = $extensible_metadata->xmp_elements($row['filepath'], $row['filename'], $overwrite, $sidecar_generated);
+    // Process image for metadata
+    while (($picture = $pictures->fetchAssoc()) !== NULL) {
+
+        // Retrieve non-empty XMP elements from image
+        $xmp_elements =
+            $extensible_metadata->xmp_elements(
+                $picture['filepath'], $picture['filename'], $overwrite, $sidecar_generated);
+
         foreach ($xmp_elements as $key => $values) {
+
+            // Get field data for current element
             $xmp_field = $extensible_metadata->get_field($key, $xmp_fields);
 
             if ($xmp_field !== NULL) {
+                // The field exists
+
+                // If the field is indexed, we need to parse search terms
                 if ($xmp_field['indexed'] === '1') {
-                    $terms = array();
+                    $search_terms = array();
+                    $insert_values = array();
+
+                    // Parse each array in the list of values, treating each unique word as an individual search term
                     foreach ($values as $value) {
                         $value = cpg_db_real_escape_string($value);
                         $term_list = explode(' ', $value);
                         $term_list = array_filter($term_list, 'strlen');
-                        $terms = array_merge($terms, $term_list);
-                        $terms = array_unique($terms);
+                        $search_terms = array_merge($search_terms, $term_list);
+                        $search_terms = array_unique($search_terms);
                     }
+
+                    // Prepare and execute insert query
+                    $table_xmp_index = $CONFIG['TABLE_PREFIX'] . 'plugin_xmp_index';
                     $id = cpg_db_real_escape_string($xmp_field['id']);
-                    $pid = cpg_db_real_escape_string($row['pid']);
-                    foreach ($terms as $term) {
+                    $pid = cpg_db_real_escape_string($picture['pid']);
+                    foreach ($search_terms as $term) {
                         $insert_values[] = "('{$pid}','{$id}','{$term}')";
                     }
-                    $table_xmp_index = $CONFIG['TABLE_PREFIX'] . 'plugin_xmp_index';
                     $term_query = "INSERT IGNORE INTO {$table_xmp_index} (`pid`, `field`, `text`) VALUES " . implode(',', $insert_values);
                     cpg_db_query($term_query);
                 }
+
             } else if (!in_array($key, $new_fields)) {
+                // The field doesn't exist yet
+                // Add it to the new fields array
                 $new_fields[] = $key;
             }
         }
+
         $images_processed++;
         if ($sidecar_generated) {
             $xmp_files_created++;
@@ -116,7 +137,7 @@ function xmp_refresh_process()
             $xmp_files_skipped++;
         }
     }
-    $result->free();
+    $pictures->free();
 
     // Insert new fields into database
     $num_fields = count($new_fields);
@@ -137,19 +158,20 @@ function xmp_refresh_process()
 
         $where .= ")";
         cpg_db_query($sql);
-    }
 
-    if ($num_fields > 0) {
         $new_xmp_fields = $extensible_metadata->xmp_fields($where);
     } else {
         $new_xmp_fields = array();
     }
 
+    // Determine if there are more images to process
+    // If not, complete the refresh process
     $more_images = ($end < $total_images);
     if ($more_images === FALSE) {
         xmp_refresh_end();
     }
 
+    // Retrieve updated last refresh information
     $xmp_status = $extensible_metadata->xmp_status();
     if ($xmp_status['last_refresh'] === NULL) {
         $last_refresh = $lang_plugin_extensible_metadata['config_never'];
@@ -157,6 +179,7 @@ function xmp_refresh_process()
         $last_refresh = localised_date($xmp_status['last_refresh_time'], $lang_date['log']);
     }
 
+    // Return status in a JSON array
     $data = array(
         'status'            => 'success',
         'new_fields'        => $new_xmp_fields,
