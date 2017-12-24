20442f0c38c14391ac151b0e650396fdecc3f5b0
Dan Paulat <dpaulat@gmail.com>
2017-12-24 00:23:57 -0700

    Add file_info hook

--- codebase.php
+++ codebase.php
@@ -12,10 +12,14 @@
 if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
 
 require_once './include/inspekt.php';
+require_once './plugins/extensible_metadata/include/initialize.inc.php';
 
 $thisplugin->add_action('page_start', 'xmp_page_start');
 
 $thisplugin->add_filter('page_meta', 'xmp_page_meta');
+$thisplugin->add_filter('file_info', 'xmp_file_info');
+$thisplugin->add_filter('search_form', 'xmp_search_form');
+$thisplugin->add_filter('thumb_caption_search', 'xmp_search_results');
 
 function xmp_plugin_id()
 {
@@ -50,4 +54,55 @@ function xmp_page_meta($var)
     $var = '<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.css" type="text/css" />' . $LINEBREAK . $var;
     $var = $var . $LINEBREAK . '<link rel="stylesheet" href="plugins/extensible_metadata/css/config.css" type="text/css" />';
     return $var;
+}
+
+function xmp_file_info($info)
+{
+    global $CURRENT_PIC_DATA;
+    global $extensible_metadata;
+    global $lang_common;
+
+    $xmp_info = array();
+
+    $overwrite = FALSE;
+    $xmp_elements = $extensible_metadata->xmp_elements(
+        $CURRENT_PIC_DATA['filepath'], $CURRENT_PIC_DATA['filename'], $overwrite, $sidecar_generated);
+    $xmp_fields = $extensible_metadata->xmp_fields("WHERE `displayed` = '1'");
+
+    foreach ($xmp_elements as $key => $elements) {
+        $field = $extensible_metadata->get_field($key, $xmp_fields);
+        if ($field !== NULL) {
+            if (!empty($field['display_name'])) {
+                $field_name = $field['display_name'];
+            } else {
+                $field_name = $field['name'];
+            }
+            foreach ($elements as $element) {
+                $xmp_links[] = '<a href="thumbnails.php?album=search&amp;xmp=on&amp;search=' . urlencode($element) . '">' . htmlspecialchars($element) . '</a>';
+            }
+            $xmp_info[$field_name] = implode('<br/>', $xmp_links);
+        }
+    }
+
+    $needle = $lang_common['filesize'];
+    $offset = array_search($needle, array_keys($info));
+    $length = count($info);
+    if ($offset === FALSE) {
+        $offset = $length;
+    }
+    $info = array_merge(array_slice($info, 0, $offset, true),
+        $xmp_info,
+        array_slice($info, $offset, $length - $offset, true));
+
+    return $info;
+}
+
+function xmp_search_form($text)
+{
+    return $text;
+}
+
+function xmp_search_results($rowset)
+{
+    return $rowset;
 }
